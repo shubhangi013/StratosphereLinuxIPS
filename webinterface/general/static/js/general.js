7f7fa1e30be46835aa38041002f86f7fbce24dcd
shubhangi013 <shubhangi.student.eee20@itbhu.ac.in>
2023-06-10 21:06:22 +0530

    enhanced navbar

--- webinterface/general/static/js/general.js
+++ webinterface/general/static/js/general.js
@@ -7,3 +7,53 @@ let table = $('#general_blockedProfilesTWs').DataTable({
     "bInfo": false,
     columns: [{ data: 'blocked' }]
 });
+
+function ResponsiveNavbar(){
+	var tabsNewAnim = $('#navbarSupportedContent');
+	var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
+	var activeItemNewAnim = tabsNewAnim.find('.active');
+	var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+	var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+	var itemPosNewAnimTop = activeItemNewAnim.position();
+	var itemPosNewAnimLeft = activeItemNewAnim.position();
+	$(".hori-selector").css({
+		"top":itemPosNewAnimTop.top + "px", 
+		"left":itemPosNewAnimLeft.left + "px",
+		"height": activeWidthNewAnimHeight + "px",
+		"width": activeWidthNewAnimWidth + "px"
+	});
+	$("#navbarSupportedContent").on("click","li",function(e){
+		$('#navbarSupportedContent ul li').removeClass("active");
+		$(this).addClass('active');
+		var activeWidthNewAnimHeight = $(this).innerHeight();
+		var activeWidthNewAnimWidth = $(this).innerWidth();
+		var itemPosNewAnimTop = $(this).position();
+		var itemPosNewAnimLeft = $(this).position();
+		$(".hori-selector").css({
+			"top":itemPosNewAnimTop.top + "px", 
+			"left":itemPosNewAnimLeft.left + "px",
+			"height": activeWidthNewAnimHeight + "px",
+			"width": activeWidthNewAnimWidth + "px"
+		});
+	});
+}
+$(document).ready(function(){
+	setTimeout(function(){ ResponsiveNavbar(); });
+});
+$(window).on('resize', function(){
+	setTimeout(function(){ ResponsiveNavbar(); }, 500);
+});
+$(".navbar-toggler").click(function(){
+	$(".navbar-collapse").slideToggle(300);
+	setTimeout(function(){ ResponsiveNavbar(); });
+});
+
+
+jQuery(document).ready(function($){
+	var path = window.location.pathname.split("/").pop();
+	if ( path == '' ) {
+		path = 'index.html';
+	}
+	var target = $('#navbarSupportedContent ul li a[href="'+path+'"]');
+	target.parent().addClass('active');
+});
